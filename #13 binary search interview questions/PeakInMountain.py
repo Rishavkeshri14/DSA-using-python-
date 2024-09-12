69c7f952b6c44dcc2e677ffe003c79a3903f5b56
Sourav kumar Keshri <128924952+Rishavkeshri14@users.noreply.github.com>
2024-09-12 13:53:36 +0530

    Update PeakInMountain.py

--- #13 binary search interview questions/PeakInMountain.py
+++ #13 binary search interview questions/PeakInMountain.py
@@ -7,17 +7,15 @@ class Solution:
     def find_pivot(self, v: List[int]) -> int:
         s = 0  # Start index of the array
         e = len(v) - 1  # End index of the array
-        mid = (s + e) // 2  # Initial midpoint
-
         # Binary search approach
         while s < e:  # Continue until start is less than end
+            mid = (s + e) // 2  # Initial midpoint 
+        
             if v[mid] < v[mid + 1]:  # If middle element is less than the next element
                 s = mid + 1  # Move the start to mid + 1
             else:
                 e = mid  # Otherwise, move the end to mid
-            
-            mid = (s + e) // 2  # Update mid after adjusting start or end
-        
+       
         return s  # Return the peak element index
 
     # Function to return the index of the peak element in a mountain array
